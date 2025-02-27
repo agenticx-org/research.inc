5159c7bcb8c401563ef8941700b2c6a6eb7ecfd0
Nick <nick.mandylas@gmail.com>
2025-02-27 18:54:23 +1100

    update(web): add youtube player

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <head>
+        <script
+          crossOrigin="anonymous"
+          src="//unpkg.com/react-scan/dist/auto.global.js"
+        />
+        {/* rest of your scripts go under */}
+      </head>
       <body className={"antialiased"}>{children}</body>
     </html>
   );
