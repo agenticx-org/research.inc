0349ee7bea88308adc9954143bab50c5f784db1c
Nick <nick.mandylas@gmail.com>
2025-03-04 23:10:45 +1100

    fix(web): logout not working

--- src/app/page-client.tsx
+++ src/app/page-client.tsx
@@ -55,8 +55,13 @@ export default function DashboardView() {
 
   const handleSignOut = async () => {
     try {
-      await authClient.signOut();
-      router.push("/"); // Redirect to home page after sign out
+      await authClient.signOut({
+        fetchOptions: {
+          onSuccess() {
+            router.push("/");
+          },
+        },
+      });
     } catch (error) {
       console.error("Error signing out:", error);
     }
