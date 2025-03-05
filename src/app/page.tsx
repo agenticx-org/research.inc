4794b44d06f407ba9f91dee8bd094b23049b0303
Nick <nick.mandylas@gmail.com>
2025-03-05 13:40:32 +1100

    fix(web): auth not working with logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import DashboardView from "./page-client";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    throw redirect("/login");
+  }
+
   return <DashboardView />;
 }
